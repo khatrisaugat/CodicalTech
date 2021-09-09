9ddbb6a86396f22b456c48ada8c0a238a6fb6933
khatrisaugat <khatri.saugat55@gmail.com>
2021-09-09 14:09:41 +0545

    about us

---
+++ src/components/Section_Card.js
@@ -0,0 +1,14 @@
+function Section_Card() {
+    return (
+        <>
+            <div className="section-card">
+                <h2>About Us</h2>
+                <p>We are a company that provide you with services of web applications and websites.We are a company that provide you with services of web applications and websites.We are a company that provide you with services of web applications and websites.We are a company that provide you with services of web applications and websites.We are a company that provide you with services of web applications and websites.</p>
+
+
+            </div>
+        </>
+    )
+}
+
+export default Section_Card
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,9 +1,19 @@
+import { Container } from "react-bootstrap"
 import Banner from "../components/Banner"
+import Section_Card from "../components/Section_Card"
 
 function Main() {
     return (
         <div className="main">
             <Banner />
+            <div className="all-body-section">
+                <Container>
+                    <Section_Card />
+                </Container>
+                <Container>
+                    <Section_Card />
+                </Container>
+            </div>
         </div>
     )
 }
